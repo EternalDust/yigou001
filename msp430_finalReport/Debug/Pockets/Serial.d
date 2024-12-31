41d26fb8be4c7a136a33bcf1e36b357c8520b30e
just bit it <143893060+JustBiteIt@users.noreply.github.com>
2024-12-31 19:39:09 +0800

    Add files via upload
    期末答辩第一版，仅处理了串口的发送和接收

---
+++ msp430_finalReport/Debug/Pockets/Serial.d
@@ -0,0 +1,39 @@
+# FIXED
+
+Pockets/Serial.obj: ../Pockets/Serial.c
+Pockets/Serial.obj: D:/ComprehensiveDesign/ccsv8/ccs_base/msp430/include/msp430.h
+Pockets/Serial.obj: D:/ComprehensiveDesign/ccsv8/ccs_base/msp430/include/msp430g2553.h
+Pockets/Serial.obj: D:/ComprehensiveDesign/ccsv8/ccs_base/msp430/include/in430.h
+Pockets/Serial.obj: D:/ComprehensiveDesign/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/intrinsics.h
+Pockets/Serial.obj: D:/ComprehensiveDesign/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/intrinsics_legacy_undefs.h
+Pockets/Serial.obj: D:/ComprehensiveDesign/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/stdint.h
+Pockets/Serial.obj: D:/ComprehensiveDesign/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/sys/stdint.h
+Pockets/Serial.obj: D:/ComprehensiveDesign/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/sys/cdefs.h
+Pockets/Serial.obj: D:/ComprehensiveDesign/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/sys/_types.h
+Pockets/Serial.obj: D:/ComprehensiveDesign/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/machine/_types.h
+Pockets/Serial.obj: D:/ComprehensiveDesign/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/machine/_stdint.h
+Pockets/Serial.obj: D:/ComprehensiveDesign/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/sys/_stdint.h
+Pockets/Serial.obj: D:/ComprehensiveDesign/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/stdarg.h
+Pockets/Serial.obj: D:/ComprehensiveDesign/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/stdio.h
+Pockets/Serial.obj: D:/ComprehensiveDesign/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/_ti_config.h
+Pockets/Serial.obj: D:/ComprehensiveDesign/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/linkage.h
+Pockets/Serial.obj: D:/ComprehensiveDesign/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/string.h
+
+../Pockets/Serial.c: 
+D:/ComprehensiveDesign/ccsv8/ccs_base/msp430/include/msp430.h: 
+D:/ComprehensiveDesign/ccsv8/ccs_base/msp430/include/msp430g2553.h: 
+D:/ComprehensiveDesign/ccsv8/ccs_base/msp430/include/in430.h: 
+D:/ComprehensiveDesign/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/intrinsics.h: 
+D:/ComprehensiveDesign/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/intrinsics_legacy_undefs.h: 
+D:/ComprehensiveDesign/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/stdint.h: 
+D:/ComprehensiveDesign/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/sys/stdint.h: 
+D:/ComprehensiveDesign/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/sys/cdefs.h: 
+D:/ComprehensiveDesign/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/sys/_types.h: 
+D:/ComprehensiveDesign/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/machine/_types.h: 
+D:/ComprehensiveDesign/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/machine/_stdint.h: 
+D:/ComprehensiveDesign/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/sys/_stdint.h: 
+D:/ComprehensiveDesign/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/stdarg.h: 
+D:/ComprehensiveDesign/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/stdio.h: 
+D:/ComprehensiveDesign/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/_ti_config.h: 
+D:/ComprehensiveDesign/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/linkage.h: 
+D:/ComprehensiveDesign/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/string.h: 
