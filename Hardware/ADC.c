2941165764fd9154d07c8aefe196807a027561d8
Soobineee <167289736+Soobineee@users.noreply.github.com>
2025-01-10 16:38:08 +0800

    BugsChange

--- Hardware/ADC.c
+++ Hardware/ADC.c
@@ -17,39 +17,39 @@ uint16_t adcbuff[SingleTrig_SIZE] = {0};
 
 void ADC_Init(void)
 {
-      /*����ADCʱ��MCLK*/
+      /*设置ADC时钟MCLK*/
       ADC10CTL1 |= ADC10SSEL_2;
-      /*ADC ����Ƶ*/
+      /*ADC 不分频*/
       ADC10CTL1 |= ADC10DIV_0;
-      /*ADC����Դʹ��TimerA_OUT1*/
+      /*ADC触发源使用TimerA_OUT1*/
       ADC10CTL1 |= SHS_1;
-      /*��ͨ������ת��ģʽ*/
+      /*单通道连续转换模式*/
       ADC10CTL1 |= CONSEQ_2;
-      /*����ADC��׼Դ*/
+      /*设置ADC基准源*/
       ADC10CTL0 |= SREF_1;
-      /*����ADC��������ʱ��16CLK*/
+      /*设置ADC采样保持时间16CLK*/
       ADC10CTL0 |= ADC10SHT_2;
-      /*����ADC������200k*/
+      /*设置ADC采样率200k*/
       ADC10CTL0 |= ADC10SR;
-      /*ADC��׼ѡ��2.5V*/
+      /*ADC基准选择2.5V*/
       ADC10CTL0 |= REF2_5V;
-      /*������׼*/
+      /*开启基准*/
       ADC10CTL0 |= REFON;
-      /*ѡ��ADC����ͨ��A4*/
+      /*选择ADC输入通道A4*/
       ADC10CTL1 |= INCH_4;
-      /*����A4ģ������*/
+      /*允许A4模拟输入*/
       ADC10AE0 |= 1 << 4;
-      /*�ж�ʹ��*/
+      /*中断使能*/
       ADC10CTL0 |= ADC10IE;
-      /*DTC����ģʽ*/
+      /*DTC传输模式*/
       //ADC10DTC0 |= ADC10CT;
-      /*�������*/
+      /*传输次数*/
       //ADC10DTC1 = 50;
-      /*��ʼ��ַ*/
+      /*起始地址*/
       //ADC10SA = (uint16_t)(adcbuff);
-      /*����ADC*/
+      /*开启ADC*/
       ADC10CTL0 |= ADC10ON;
-      /*����ת��*/
+      /*允许转换*/
       ADC10CTL0 |= ENC;
 }
 
@@ -57,20 +57,20 @@ void ADC_Init(void)
 
 void TimerA0_Init(void)
 {
-    /*����ʱ��ԴΪSMCLK*/
+    /*设置时钟源为SMCLK*/
     TA0CTL |= TASSEL_2;
-    /*���ù���ģʽΪUp&Down*/
+    /*设置工作模式为Up&Down*/
     TA0CTL |= MC_1;
-    /*����TA1CCR0Ϊ0x00FF*/
+    /*设置TA1CCR0为0x00FF*/
     TA0CCR0 = 0x00FF;
-    /*����TA1CCR1Ϊ0x00FF*/
-    TA0CCR1 = 0x007F;//ռ�ձ�(TACCR0 - TACCR1) / TACCR0,Ƶ��=SMCLK/(TACCR0+1)/2
-    /*����Ϊ�Ƚ�ģʽ*/
+    /*设置TA1CCR1为0x00FF*/
+    TA0CCR1 = 0x007F;//占空比(TACCR0 - TACCR1) / TACCR0,频率=SMCLK/(TACCR0+1)/2
+    /*设置为比较模式*/
     TA0CCTL0 &= ~CAP;
     TA0CCTL1 &= ~CAP;
-    /*���ñȽ����ģʽ*/
+    /*设置比较输出模式*/
     TA0CCTL1 |= OUTMOD_6;
-    /*����IO����*/
+    /*设置IO复用*/
     P1SEL |= BIT2;
     P1DIR |= BIT2;
 }
@@ -93,9 +93,9 @@ void ADCConvert_Start(void)
 }
 
 
-//��ѹ��������ģʽ
-//ADC�����뱾�ļ�����һ�£�ͬʱ��ҪTimerA0_Init();
-//��Ҫ����while(1)ѭ����
+//电压连续触发模式
+//ADC配置与本文件保持一致，同时需要TimerA0_Init();
+//需要放在while(1)循环中
 void ADC_AutoTrig(void)
 {
     if (ADC10CTL0 & ADC10IFG) 
@@ -107,27 +107,27 @@ void ADC_AutoTrig(void)
     while (read_index != write_index) 
     {
         uint16_t data = ring_buffer[read_index];
-        uint16_t ppval = cal_ppvalue(ring_buffer);//���ֵ
-        uint16_t ave = average_array(ring_buffer);//��ֵ
-        uint16_t f = frequency(ring_buffer,BUFFER_SIZE);//Ƶ��
+        uint16_t ppval = cal_ppvalue(ring_buffer, BUFFER_SIZE);//峰峰值
+        uint16_t ave = average_array(ring_buffer, BUFFER_SIZE);//均值
+        uint16_t f = frequency(ring_buffer,BUFFER_SIZE);//频率
 
         read_index = (read_index + 1) % BUFFER_SIZE; 
         Serial_printf("%d, %d, %d, %d, %d\n", data, (data * 2500L) / 1023, (ppval/* * 2500L*/)/* / 1023*/, (ave/* * 2500L*/)/* / 1023*/, f);
-        //��ע������Ϊ��ȷ����Ҫԭʼ���ݻ���ת���������
+        //加注释是因为不确定是要原始数据还是转换后的数据
         __delay_cycles(30000);
     }
 }
 
 
 
-//��ѹ���δ���ģʽ
-//ADC������Ҫ��DTCģʽ����ע��ȥ�����ɣ�ͬ�������ɼ���ע�ͼ��ϼ���
+//电压单次触发模式
+//ADC配置需要打开DTC模式，将注释去掉即可，同样连续采集将注释加上即可
 void ADC_SingleTrig(void)
 {
     for (int i = 0; i < SingleTrig_SIZE; i++)
     {
-        uint16_t ppval = cal_ppvalue(adcbuff);//���ֵ
-        uint16_t ave = average_array(adcbuff);//��ֵ
+        uint16_t ppval = cal_ppvalue(adcbuff, 50);//峰峰值
+        uint16_t ave = average_array(adcbuff, 50);//均值
         Serial_printf("%d, %d, %d, %d,\n", *(adcbuff+i), (*(adcbuff+i) * 2500L) / 1023, (ppval/* * 2500L*/)/* / 1023*/, (ave/* * 2500L*/)/* / 1023*/);
     }
 }
